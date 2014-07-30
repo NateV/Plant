cb31390b2be462e68bbaa2f80be94eca83439e0d
NateV <nathan.c.vogel@gmail.com>
2014-07-29 21:45:33 -0400

    added recursive structure

--- public/scripts/aGarden.js
+++ public/scripts/aGarden.js
@@ -1,12 +1,52 @@
-var myPath = new Path();
-myPath.strokeColor = 'black';
-myPath.add(new Point(0,0), new Point(0,50), new Point(25, 25));
-myPath.closed = true;
-myPath.fullySelected = true;
 
-triangle = myPath.clone();
-triangle.position.x += 100;
-triangle.smooth();
+//CLASSES 
+function Leaf (base) {
+  this.base = base;
+  this.parentid;
+  this.child;
+  
+  this.id = 0;
+  
+  this.sides = new Array();
+  
+  base.strokeColor = "black";
+  
+  this.grow = function (event) {
+    if (this.child==null) {
+      if (event.count % 10==0) {
+      
+        //create the new child
+        var kidbase = this.base.clone(); 
+        kidbase.position -= 10;
+        this.child = new Leaf(kidbase);
+        
+        //give the child an ID and a pointer to its parent
+        this.child.id = this.id + 1;
+        this.child.parentid = this.id;
+      }
+    } else {
+      this.child.grow(event)
+    }
+    
+  }
+}//end of Leaf
 
-alsoTriangle = new Path.RegularPolygon(new Point(50, 100), 3, 35);
-alsoTriangle.fillColor = 'black';
+
+
+
+//LOOP.
+function onFrame(event) {
+//  console.log("Time:" + event.time.toFixed());
+//  seed.grow()
+}//end of onFrame
+
+//spot = new Point(view.size.width/2, view.size.height);
+base = new Path(new Point(view.size.width/2 - 20, view.size.height - 20), new Point(view.size.width/2 + 20, view.size.height - 20))
+seed = new Leaf(base);
+
+//test loop.
+for (i=0;i<50; i++) {
+  seed.grow({count: i});
+}
+
+console.log(seed);
